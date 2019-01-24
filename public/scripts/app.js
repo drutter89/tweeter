b4d898233480bee1a808927698ddac593aaf2541
Dylan Rutter <dylanmrutter89@gmail.com>
2019-01-24 02:16:25 +0000

    got form validation working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,52 +7,52 @@ $(document).ready(function() {
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
+// const tweetData = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": {
+//           "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//           "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//         },
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": {
+//           "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+//           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+//           "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+//         },
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     },
+//     {
+//       "user": {
+//         "name": "Johann von Goethe",
+//         "avatars": {
+//           "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+//           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+//           "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+//         },
+//         "handle": "@johann49"
+//       },
+//       "content": {
+//         "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+//       },
+//       "created_at": 1461113796368
+//     }
+//   ];
 
   
   // Test / driver code (temporary)
@@ -112,13 +112,14 @@ $('#tweet-form').on('submit', function (event) {
     console.log("Testing for DATA here",newTweetData); // name=spot&breed=corgi
   
     // let newDogName = $('input[name=dog-name]').val()
-  
+    if (newTweetData === '' || newTweetData === null) {
+        alert("You must type something");
+    } else if (newTweetData.length > 140){
+        alert("You can only type 140 characters");
+    }
     postTweet(newTweetData);
   
   })
-
-
-  
 
   function loadTweets () {
     $.get('http://localhost:8080/tweets')
@@ -137,7 +138,7 @@ $('#tweet-form').on('submit', function (event) {
     //   let tweetElement = createTweetElement(tweet);
     //   console.log("This is my tweet element", tweetElement);
     //   renderTweet(tweetElement);
-      loadTweets();
+    //   loadTweets();
     })
   }
 
@@ -152,8 +153,6 @@ $('#tweet-form').on('submit', function (event) {
 //       });
 //     });
 //   });
-
-
 
 })
 
